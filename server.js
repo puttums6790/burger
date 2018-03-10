225f6cdbfa65414aa716b26d8ba3c8e8b3699074
Jack Kennedy <puttums6790@gmail.com>
2018-03-10 09:37:22 -0600

    jaws

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
-
 var routes = require("./controllers/burgers_controller.js");
-
+var methodOverride = require("method-override");
 var PORT = process.env.PORT || 3000;
-
 var app = express();
 
 
